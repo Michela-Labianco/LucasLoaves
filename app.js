e8591212bee62ec99f4f6750ed7694a2364298de
Michela <michela@Michelas-MacBook-Pro.local>
2025-08-06 14:03:22 +1000

    connection changed

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(session({
 }));
 
 // Connect to MongoDB (database)
-mongoose.connect('mongodb://localhost:27017/lucasloaves')
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('✅ MongoDB connected'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
